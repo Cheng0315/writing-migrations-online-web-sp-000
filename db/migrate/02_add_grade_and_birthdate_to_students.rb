9fa65eb1e4820d885bc96110a278bc5d4d4a057c
Cheng Vue <cvu3@ucsc.edu>
2018-12-28 20:32:18 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class Add_grade_and_birthdate_to_students < ActiveRecord::Migration
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
   def change
     add_column :student, :grade, :integer
     add_column :student, :birthdate, :string
